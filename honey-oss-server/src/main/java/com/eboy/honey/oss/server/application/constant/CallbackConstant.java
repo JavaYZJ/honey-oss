4ed8bc005cad4d964503d06781ec82985de0a33e
yzj <869737937@qq.com>
2020-10-10 17:56:25 +0800

    [bugFix] 修复多用户同时上传同个文件时，上传进度慢的用户会由于前面上传速度快的原因导致主键冲突的bug

---
+++ honey-oss-server/src/main/java/com/eboy/honey/oss/server/application/constant/CallbackConstant.java
@@ -0,0 +1,14 @@
+package com.eboy.honey.oss.server.application.constant;
+
+/**
+ * @author yangzhijie
+ * @date 2020/10/10 14:59
+ */
+public class CallbackConstant {
+
+    public final static String CALL_BACK_SUCCESS_MSG = "success";
+
+    public final static int CALL_BACK_SUCCESS_CODE = 200;
+
+    public final static int CALL_BACK_FAIL_CODE = 500;
+}
